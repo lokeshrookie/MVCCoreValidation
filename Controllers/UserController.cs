14b4458114ef101c25803e15b82f22f70ebc4aaa
lokeshrookie <rocklinglokesh@gmail.com>
2024-06-09 18:16:42 +0530

    Initial Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using DataValidations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataValidations.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Index(UserModel model)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        return RedirectToAction("Success");
+        //    }
+        //    return View(model);
+        //}
+
+        [HttpPost]
+        public IActionResult Index(UserModel model)
+        {
+            if (model.HighestEducation == "Engineering" && string.IsNullOrEmpty(model.EngineeringBranch))
+            {
+                ModelState.AddModelError("EngineeringBranch", "Engineering Branch is required if Highest Education is Engineering");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Process the valid model
+                return RedirectToAction("Success");
+            }
+
+            // Return the view with validation errors
+            return View(model);
+        }
+
+        public IActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataValidations.Models
+{
+    public class UserModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Emial { get; set; }
+
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [DataType(DataType.Date)]
+        [CustomValidation(typeof(UserModel), "ValidateAge")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "Gender is required")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Highest Education is required")]
+        public string HighestEducation { get; set; }
+                                                          
+        
+        public string EngineeringBranch { get; set; }
+
+        public static ValidationResult ValidateAge(DateTime dateOfBirth, ValidationContext context)
+        {
+            var age = DateTime.Now.Year - dateOfBirth.Year;
+            if (dateOfBirth > DateTime.Now.AddYears(-age)) age--;
+            return age >= 18 ? ValidationResult.Success : new ValidationResult("Age must be 18 or older");
+        }
+
+
+
+    }
+}

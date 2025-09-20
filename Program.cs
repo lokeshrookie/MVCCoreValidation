135a21688ec78101aea8682d148b87d2ef40b338
lokeshrookie <rocklinglokesh@gmail.com>
2025-09-20 23:06:37 +0530

    Add solution file

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<YourDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
